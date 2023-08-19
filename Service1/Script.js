349358f4580d4260e880ce67d61763e47ce77ac9
Arsalan Ali <129784950+RajputAli@users.noreply.github.com>
2023-08-19 19:21:02 +0500

    Add files via upload

---
+++ Service1/Script.js
@@ -0,0 +1,17 @@
+
+
+// const changeColor = (event)=>{
+//     console.log(event)
+//     if (event){
+//         let div = document.getElementById("color")
+//         div.classList.remove("display-flex3")
+//         div.classList.add("display-flex1")
+//     }
+// }
+
+// const backcolor = ()=>{
+//     let div = document.getElementById("color")
+//     div.classList.remove("display-flex1")
+//     div.classList.add("display-flex3")
+// }
+
